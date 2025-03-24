9eed54b20048878df8aa1ab74fb6ffb1584a40bd
Donartkr <donartkrasniqi23@gmail.com>
2025-03-24 19:48:36 +0100

    module 28.html

---
+++ Module 28/book-examples/event2.js
@@ -0,0 +1,19 @@
+var button2 =document.getElementById('btn2');
+var button3=document.getElementById('btn3');
+var button4=document.getElementById('btn4');
+
+
+button2.onmouseover = function ()
+{
+    alert('hello');
+}
+
+button3.onmouseover = function ()
+{
+    alert('hello');
+}
+
+button4.onmouseover = function ()
+{
+    alert('hello');
+}
---
+++ Module 28/book-examples/events2.js
@@ -0,0 +1,11 @@
+var btn1 = document.getElementById('btn1');
+
+
+btn1.addEventListener('click',function () {
+alert('hello');
+
+});
+
+
+
+
