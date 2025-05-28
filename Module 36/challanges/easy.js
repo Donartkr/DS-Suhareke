4a1936edf7ae6fcfb91a81aa518e7c0324420f21
Donartkr <donartkrasniqi23@gmail.com>
2025-05-28 19:17:03 +0200

    challanges

---
+++ Module 36/challanges/easy.js
@@ -0,0 +1,15 @@
+$(window).ready(function(){
+    alert('jQuery loaded successfully!');
+});
+
+$('#header');
+$('li');
+$('ul li');
+$('.student');
+
+$('a.test:first');
+$('tr:odd');
+$('#myForm :input');
+$('div:visible');
+$('div:gt(2)');
+$('div:animated');
